86a7c5c7beff80e2553c9c7057f948a6ab7415ca
Vanced Solutions <145443805+satnamsinghvanced@users.noreply.github.com>
2024-09-04 18:49:36 +0530

    REFACTOR CODE

--- src/api/routes/contactUs/index.js
+++ src/api/routes/contactUs/index.js
@@ -1,22 +1,22 @@
 import express from "express";
-import pool from "../../../db/index.js";
-import { createTables } from "../../helpers/createTable.js";
+// import pool from "../../../db/index.js";
+// import { createTables } from "../../helpers/createTable.js";
 import sendContactEmail from "../../helpers/sendEmail.js";
 const router = express.Router();
 
 router.post("/", async (req, res) => {
   try {
-    await createTables();
+    // await createTables();
     const { name, email, phoneNumber, reason, comments } = req.body;
-    const connection = await pool.getConnection();
-    await connection.beginTransaction();
-    const [contactForm] = await connection.query(
-      `INSERT INTO contactForm (name, email, phoneNumber, reason, comments) VALUES (?, ?, ?, ?, ?)`,
-      [name, email, phoneNumber, reason, comments]
-    );
+    // const connection = await pool.getConnection();
+    // await connection.beginTransaction();
+    // const [contactForm] = await connection.query(
+    //   `INSERT INTO contactForm (name, email, phoneNumber, reason, comments) VALUES (?, ?, ?, ?, ?)`,
+    //   [name, email, phoneNumber, reason, comments]
+    // );
     sendContactEmail(name, email, phoneNumber, reason, comments);
-    console.log(contactForm);
-    connection.release();
+    // console.log(contactForm);
+    // connection.release();
     res.status(201).json({ message: "Data uploaded successfully" });
   } catch (err) {
     res.status(500).json({ msg: "Server error" });
