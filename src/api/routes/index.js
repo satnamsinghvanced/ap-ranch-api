69b47c7dac940daf05d7b3453bee40e2e801b5fe
Vanced Solutions <145443805+satnamsinghvanced@users.noreply.github.com>
2024-09-04 18:51:27 +0530

    Update index.js

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -1,9 +1,9 @@
 import express from "express";
-import home from "./home/index.js";
+// import home from "./home/index.js";
 import contact from "./contactUs/index.js";
-import service from "./services/index.js";
+// import service from "./services/index.js";
 const router = express.Router();
-router.use("/home", home);
+// router.use("/home", home);
 router.use("/contact", contact);
-router.use("/service", service);
+// router.use("/service", service);
 export default router;
