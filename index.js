e7e85672112450e50bc72bc9ecaebe32d93e26c1
Vanced Solutions <145443805+satnamsinghvanced@users.noreply.github.com>
2024-09-05 16:16:08 +0530

    refactor the code

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import config from "./config.js";
 import path from "path";
 import { fileURLToPath } from "url";
 //import "./src/db/index.js";
-// import pool from "./src/db/index.js";
+import pool from "./src/db/index.js";
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -28,15 +28,15 @@ app.use("/api", routes);
 
 app.use("/public", express.static("./public"));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-// pool
-//   .query("SELECT 1")
-//   .then(() => {
-//     console.log("Connected to MySQL");
+pool
+  .query("SELECT 1")
+  .then(() => {
+    console.log("Connected to MySQL");
     app.listen(PORT, () => {
       console.log("Server is running..." + PORT);
     });
-  // })
-  // .catch((error) => {
-  //   console.error("Unable to connect to MySQL:", error.message);
-  //   process.exit(1);
-  // });
+  })
+  .catch((error) => {
+    console.error("Unable to connect to MySQL:", error.message);
+    process.exit(1);
+  });
--- src/api/routes/contactUs/index.js
+++ src/api/routes/contactUs/index.js
@@ -1,21 +1,20 @@
 import express from "express";
-// import pool from "../../../db/index.js";
+import pool from "../../../db/index.js";
 import sendContactEmail from "../../helpers/sendEmail.js";
 const router = express.Router();
 
 router.post("/", async (req, res) => {
   try {
     const { name, email, phoneNumber, reason, comments } = req.body;
-    // const connection = await pool.getConnection();
-    // await connection.beginTransaction();
-    // const [contactForm] = await connection.query(
-    //   `INSERT INTO contactForm (name, email, phoneNumber, reason, comments) VALUES (?, ?, ?, ?, ?)`,
-    //   [name, email, phoneNumber, reason, comments]
-    // );
+    const connection = await pool.getConnection();
+    await connection.beginTransaction();
+    const [contactForm] = await connection.query(
+      `INSERT INTO contactForm (name, email, phoneNumber, reason, comments) VALUES (?, ?, ?, ?, ?)`,
+      [name, email, phoneNumber, reason, comments]
+    );
     await sendContactEmail(name, email, phoneNumber, reason, comments);
-    // console.log(contactForm);
-    //await connection.commit();
-    // connection.release();
+    await connection.commit();
+    connection.release();
     res.status(201).json({ message: "Data uploaded successfully" });
   } catch (err) {
     res.status(500).json({ msg: "Server error" });
